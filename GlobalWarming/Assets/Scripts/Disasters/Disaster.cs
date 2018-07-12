b56a507e31ee304b51c41512f468bac3a7731825
DarrenHobern <darren.hobern@gmail.com>
2018-07-12 17:31:42 +1200

    Add base for disasters and disaster spawner

---
+++ GlobalWarming/Assets/Scripts/Disasters/Disaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// TODO make strenght-weakness table so picking a strength automatically indicates its weaknesses.
+///      Also disallow picking clashing elements... maybe
+/// </summary>
+[System.Serializable]
+public class Element
+{
+    [Header("Element Type")]
+    public bool fire;
+    public bool water;
+    public bool earth;
+    public bool air;
+    public bool ice;
+    public bool disease;
+    public bool electricity;
+
+}
+
+public class Disaster : MonoBehaviour {
+
+    [SerializeField] Element element;
+
+
+
+}
