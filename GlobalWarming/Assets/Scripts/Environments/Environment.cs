84fd4804c7d0c210ace8916fb062709a4d8f305f
DarrenHobern <darren.hobern@gmail.com>
2018-07-15 13:29:48 +1200

    Create temp Forest and City tiles
    Started making the tile prefabs for the map.
    Using temp ones made from the library to save time, actual ones will be
    3d models.
    
    Also updated the map generater to use the prefabs rather than materials.

---
+++ GlobalWarming/Assets/Scripts/Environments/Environment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileStatus
+{
+    OPTIMAL,
+    FUNCTIONAL,
+    DAMAGED,
+    DESTROYED
+}
+
+public enum EnvironmentType
+{
+    URBAN,
+    RURAL,
+    WILDERNESS
+}
+
+public class Environment : MonoBehaviour {
+
+    Dictionary<Element, float> ElemResistance;
+    [SerializeField] int population;
+    [SerializeField] string regionName;
+    [SerializeField] int health;
+    [SerializeField] EnvironmentType type = EnvironmentType.WILDERNESS;
+    private TileStatus status = TileStatus.OPTIMAL;
+
+    public string GetEnvironmentType()
+    {
+        return type.ToString();
+    }
+}
--- GlobalWarming/Assets/Scripts/GameHexMapGenerator.cs
+++ GlobalWarming/Assets/Scripts/GameHexMapGenerator.cs
@@ -4,63 +4,78 @@ using UnityEngine;
 using Wunderwunsch.HexMapLibrary;
 using Wunderwunsch.HexMapLibrary.Generic;
 
-namespace Wunderwunsch.HexMapLibrary.Examples
-{ 
-    /// <summary>
-    /// Example 1: Mouse Position marker for tile, closest edge and closest corner as well as simple map manipulation (left click to change tile value)
-    /// </summary>
-    public class GameHexMapGenerator : MonoBehaviour
+public class GameHexMapGenerator : MonoBehaviour
+{
+    //you will find the prefabs and materials used here in the "NonScriptAssets folder or of the package.
+    [SerializeField] private int mapSize = 10; // the mapSize, can be set in inspector 
+    [SerializeField] private GameObject tilePrefab = null; // the prefab we use for each Tile -> use TilePrefab.prefab 
+    [SerializeField] private GameObject tileMarker = null; // a GameObject we use to show the current mouse position -> create an instance of TileMarker.prefab and reference it in the inspector
+
+    [SerializeField] private List<GameObject> tilePrefabs = null;
+    
+
+    //[SerializeField] private List<Material> materials = null; // the materials we want to assign to the tiles for visualisation purposes -> set size to 4 in inspector and add TileMat1 to TileMat4
+    private HexMap<int,bool> hexMap; // our map. For this example we create a map where an integer represents the data of each tile and a bool the data of each edge
+    private GameObject[] tileObjects; // this will contain all the GameObjects for visualisation purposes, their array index corresponds with the index of our Tiles
+
+    [SerializeField] private HexPlayerPosition hexPlayer = null;
+
+    void Start ()
     {
-        //you will find the prefabs and materials used here in the "NonScriptAssets folder or of the package.
-        [SerializeField] private int mapSize = 10; // the mapSize, can be set in inspector 
-        [SerializeField] private GameObject tilePrefab = null; // the prefab we use for each Tile -> use TilePrefab.prefab 
-        [SerializeField] private GameObject tileMarker = null; // a GameObject we use to show the current mouse position -> create an instance of TileMarker.prefab and reference it in the inspector
-        [SerializeField] private List<Material> materials = null; // the materials we want to assign to the tiles for visualisation purposes -> set size to 4 in inspector and add TileMat1 to TileMat4
-        private HexMap<int,bool> hexMap; // our map. For this example we create a map where an integer represents the data of each tile and a bool the data of each edge
-        private GameObject[] tileObjects; // this will contain all the GameObjects for visualisation purposes, their array index corresponds with the index of our Tiles
+        Debug.Assert(tilePrefabs != null);
 
-        [SerializeField] private HexPlayerPosition hexPlayer = null;
+        hexMap = new HexMap<int, bool>(HexMapBuilder.CreateHexagonalShapedMap(mapSize), null);  //creates a HexMap using one of the pre-defined shapes in the static MapBuilder Class  
+        hexPlayer.Init(hexMap);
+        tileObjects = new GameObject[hexMap.TilesByPosition.Count]; //creates an array with the size equal to the number on tiles of the map
 
-        void Start ()
+        foreach (var tile in hexMap.Tiles)
         {
-            hexMap = new HexMap<int, bool>(HexMapBuilder.CreateHexagonalShapedMap(mapSize), null);  //creates a HexMap using one of the pre-defined shapes in the static MapBuilder Class  
-            hexPlayer.Init(hexMap);
-            tileObjects = new GameObject[hexMap.TilesByPosition.Count]; //creates an array with the size equal to the number on tiles of the map
-
-            foreach (var tile in hexMap.Tiles) //loops through all the tiles, assigns them a random value and instantiates and positions a gameObject for each of them.
-            {
-                tile.Data = (Random.Range(0, 4));
-                GameObject instance = GameObject.Instantiate(tilePrefab, transform);
-                instance.GetComponent<Renderer>().material = materials[tile.Data];
-                instance.name = "MapTile_" + tile.Position;
-                instance.transform.position = tile.CartesianPosition;
-                tileObjects[tile.Index] = instance;
-            }
-
+            tile.Data = Random.Range(0, tilePrefabs.Count);
+            GameObject instance = Instantiate(tilePrefabs[tile.Data], transform);
+            instance.name = instance.name + "_" + tile.Position;
+            instance.transform.position = tile.CartesianPosition;
+            tileObjects[tile.Index] = instance;
         }
 
+        /* OLD
+        foreach (var tile in hexMap.Tiles) //loops through all the tiles, assigns them a random value and instantiates and positions a gameObject for each of them.
+        {
+            tile.Data = (Random.Range(0, 4));
+            GameObject instance = GameObject.Instantiate(tilePrefab, transform);
+            instance.GetComponent<Renderer>().material = materials[tile.Data];
+            instance.name = tilePrefab.name + "_" + tile.Position;
+            instance.transform.position = tile.CartesianPosition;
+            tileObjects[tile.Index] = instance;
+        }
+        */
+    }
 
-        void Update ()
-        { 
-            if (!hexPlayer.CursorIsOnMap) return; // if we are not on the map we won't do anything so we can return
 
-            Vector3Int mouseTilePosition = hexPlayer.TileCoord;
-            //Vector3Int mouseEdgePosition = hexMouse.ClosestEdgeCoord;
-            //Vector3Int mouseCornerPosition = hexMouse.ClosestCornerCoord;
+    void Update ()
+    { 
+        if (!hexPlayer.CursorIsOnMap) return; // if we are not on the map we won't do anything so we can return
 
-            //update the marker positions
-            tileMarker.transform.position = HexConverter.TileCoordToCartesianCoord(mouseTilePosition, 0.1f); //we put our tile marker on the tile our mouse is on
-            //edgeMarker.transform.position = HexConverter.EdgeCoordToCartesianCoord(mouseEdgePosition); // we put our edge marker on the closest edge of our mouse position            
-            //edgeMarker.transform.rotation = Quaternion.Euler(0, hexMap.EdgesByPosition[mouseEdgePosition].EdgeAlignmentAngle, 0); //we set the rotation of the edge marker
-            //cornerMarker.transform.position = HexConverter.CornerCoordToCartesianCoord(mouseCornerPosition);
+        Vector3Int playerTilePosition = hexPlayer.TileCoord;
 
-            if (Input.GetMouseButtonDown(0)) // change a tile when clicked on it
-            {
-                Tile<int> t = hexMap.TilesByPosition[mouseTilePosition]; //we select the tile our mouse is on
-                int curValue = t.Data; //we grab the current value of the tile
-                t.Data = ((curValue + 1) % 4); //we increment it and use modulo to keep it between 0 and 3
-                tileObjects[t.Index].GetComponent<Renderer>().material = materials[t.Data]; // we update the material of the GameObject representing the tile based on the new value
-            }
+        //Vector3Int mouseEdgePosition = hexMouse.ClosestEdgeCoord;
+        //Vector3Int mouseCornerPosition = hexMouse.ClosestCornerCoord;
+
+        //update the marker positions
+        tileMarker.transform.position = HexConverter.TileCoordToCartesianCoord(playerTilePosition, 0.1f); //we put our tile marker on the tile our mouse is on
+        //edgeMarker.transform.position = HexConverter.EdgeCoordToCartesianCoord(mouseEdgePosition); // we put our edge marker on the closest edge of our mouse position            
+        //edgeMarker.transform.rotation = Quaternion.Euler(0, hexMap.EdgesByPosition[mouseEdgePosition].EdgeAlignmentAngle, 0); //we set the rotation of the edge marker
+        //cornerMarker.transform.position = HexConverter.CornerCoordToCartesianCoord(mouseCornerPosition);
+
+        if (Input.GetMouseButtonDown(0)) // change a tile when clicked on it
+        {
+            Tile<int> t = hexMap.TilesByPosition[playerTilePosition]; // select the tile the player is looking at
+            
+            //Tile<int> t = hexMap.TilesByPosition[mouseTilePosition]; //we select the tile our mouse is on
+            //int curValue = t.Data; //we grab the current value of the tile
+            //t.Data = ((curValue + 1) % 4); //we increment it and use modulo to keep it between 0 and 3
+            
+            //tileObjects[t.Index].GetComponent<Renderer>().material = materials[t.Data]; // we update the material of the GameObject representing the tile based on the new value
         }
     }
 }
+
